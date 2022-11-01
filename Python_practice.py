4dad528fb128ab32617a4ed42501b3eb82f5b1c9
tanahildebrand <tana.hildebrand@gmail.com>
2022-11-01 18:43:39 -0400

    added VS files

---
+++ Python_practice.py
@@ -0,0 +1,5 @@
+counties = ["Arapahoe","Denver","Jefferson"]
+if "Arapahoe" in counties and "El Paso" not in counties:
+   print("Only Arapahoe is in the list of counties.")
+else:
+    print("Arapahoe is in the list of counties and El Paso is not in the list of counties.")
