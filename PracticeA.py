9e3f1bf127fef2e89ab891a70f727c47cdde390b
tanahildebrand <tana.hildebrand@gmail.com>
2022-11-07 16:06:57 -0500

    removing extra files

--- PracticeA.py
+++
@@ -1,2 +0,0 @@
-import csv
-dir(csv)
--- Python_practice.py
+++
@@ -1,8 +0,0 @@
-counties = ["Arapahoe","Denver","Jefferson"]
-if "Arapahoe" in counties and "El Paso" not in counties:
-   print("Only Arapahoe is in the list of counties.")
-else:
-    print("Arapahoe is in the list of counties and El Paso is not in the list of counties.")
-
-for county in counties:
-   print(county)
