bf56daf67ea7426468f54a6250422ed5c9842a58
tanahildebrand <tana.hildebrand@gmail.com>
2022-11-07 11:18:14 -0500

    vs files

---
+++ SkillDrill2.py
@@ -0,0 +1,3 @@
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829}, {"county":"Denver", "registered_voters": 463353}, {"county":"Jefferson", "registered_voters": 432438}]
+for county,voters in voting_data.items():
+    print(f"{county} county has {voters:,} registered voters.")
